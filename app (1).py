9ea7f57889230062c6021ecdea94b52d613d151b
runcis <rinalds1234@gmail.com>
2019-03-05 22:41:30 +0200

    python caluclator app

---
+++ app (1).py
@@ -0,0 +1,28 @@
+print("Heloo bro, This is Davis calculator, try and break it!")
+num1=int(input("enter your first number - "))
+func=input("function - + , - , * , / ")
+num2=int(input("enter your second number - "))
+
+if func== "+":
+    ans=num1+num2
+    print(ans)
+else:
+    pass
+
+if func== "-":
+    ans=num1-num2
+    print(ans)
+else:
+    pass
+
+if func== "*":
+    ans=num1*num2
+    print(ans)
+else:
+    pass
+
+if func== "/":
+    ans=num1/num2
+    print(ans)
+else:
+    pass
